b3eb7e3260e99c57b77ce07e8d01c09177842d5c
Md Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2022-10-12 13:58:58 +0800

    Add boy names

--- babynames_crawler/babynames_crawler/spiders/babynames_spider.py
+++ babynames_crawler/babynames_crawler/spiders/babynames_spider.py
@@ -7,17 +7,19 @@ class BabynamesSpider(scrapy.Spider):
         urls = []
         for letter in 'abcdefghijklmnopqrstuvwxyz':
             urls.append(f'https://hamariweb.com/names/muslim-girl-names-starting-with-{letter}/page-1')
+            urls.append(f'https://hamariweb.com/names/muslim-boy-names-starting-with-{letter}/page-1')
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        gender = 'boy' if 'boy' in response.url.split('/')[-2] else 'girl'
         rows = response.xpath("//table[@class='table']/tr[not(@class='tbl_header')]")
         for row in rows:
             yield {
                 'name': row.xpath('./td/a/text()').get(),
                 'meaning': row.xpath('./td/text()').get(),
-                'gender': 'girl',
+                'gender': gender,
                 'ethnicity': "muslim"
             }
 
